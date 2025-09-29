c58f7e92de7903f90190af479d3623234dba358f
mdsharimalam <mdsharimalam19@gmail.com>
2025-09-29 19:23:17 +0530

    props concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,7 +1,16 @@
-
+import Contact from "./Contact";
+import Greeting from "./Greeting";
+import Home from "./Home";
 function App() {
   return (
+    <div>
    <h1>Hello From React Basic projects</h1>
+   <Greeting name = 'Sharim' />
+   <Greeting name = 'Shadab' />
+   <Greeting name = 'Saif' />
+   {/* <Home />
+   <Contact /> */}
+   </div>
   );
 }
 
---
+++ basics/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <>
+        <h3>Header Component</h3>
+        <header>Some Header Content</header>
+        </>
+    )
+}
+export default Header;
